06797c64a1f14253c289c21d966484b100554aff
Debdulal Mahata <76114290+debdulal31@users.noreply.github.com>
2025-11-01 22:32:27 +0530

    tailwind added

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+    extend: {
+    fontFamily: {
+      myfont: ['MyFont', 'sans-serif'],
+    },
+  },
+  plugins: [],
+}
